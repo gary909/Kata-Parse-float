867acdfbc307fba7fc6f609ded66df48e680a05e
gary909 <garywhite61@hotmail.com>
2023-04-06 15:49:51 +0200

    code working

--- app.js
+++ app.js
@@ -1,5 +1,11 @@
-function parseF(s) {
-    return parseFloat(s);
-}
-
+function parseF(s){
+    let n = parseFloat(s);
+    if (Number.isNaN(n)) {
+    n = null;
+  }
+    return n;
+  }
+  
 console.log(parseF("1")); // 1.0
+console.log(parseF("s")); // null
+console.log(parseF("2.5")); // 2.5
